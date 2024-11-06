bcd1272e63202e4ff214aa9c6da71695514a308f
zlac480 <zlac480@live.rhul.ac.uk>
2024-11-06 14:13:16 +0000

    finishing the round() and runRound() methods, adding appropriate method comments

--- product/src/main/java/main/BanditSolverExploreOnly.java
+++ product/src/main/java/main/BanditSolverExploreOnly.java
@@ -15,6 +15,9 @@ public class BanditSolverExploreOnly {
   int currentRound;
   Random random;
 
+  /*
+   * Initialised variables above and taking list arms and totalRounds from main when set. 
+   */
   public BanditSolverExploreOnly(List<BanditArm> arms, int totalRounds) {
     this.arms = arms;
     this.totalRounds = totalRounds;
@@ -23,16 +26,33 @@ public class BanditSolverExploreOnly {
     this.random = new Random();
   }
 
+  /*
+   * Selects a random arm from the list arms.
+   */
   public BanditArm selectArm() {
-    BanditArm arm = new BanditArm();
-    return arm;
+    int randomIndex = random.nextInt(arms.size());
+    return arms.get(randomIndex);
   }
 
-  public void Round(BanditArm arm) {
-
+  /*
+   * Runs one round, selecting an arm using selectArm(), getting the points 
+   * and updating relevant values.
+   */
+  public void round() {
+    if (currentRound < totalRounds) {
+      BanditArm arm = selectArm();
+      arm.incrimentVisits();
+      totalPoints += arm.getPoints();
+      currentRound++;
+    }
   }
   
+  /*
+   * Checks whether there are more rounds to be run and then runs them.
+   */
   public void runRound() {
-    
+    while (currentRound < totalRounds) {
+      round();
+    }
   }
 }
