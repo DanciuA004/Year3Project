0710df6195511d88f84b57dd7cf7535b853cd03e
zlac480 <zlac480@live.rhul.ac.uk>
2024-11-06 14:06:49 +0000

    added constructor to take list arms and total rounds from main when initialised. Changed updateArm to Round, that will similate one round and runRound that will compare totalRounds and currentRound to run more rounds

--- product/src/main/java/main/BanditSolverExploreOnly.java
+++ product/src/main/java/main/BanditSolverExploreOnly.java
@@ -2,22 +2,37 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /*
- * 
+ * This class solves the bandit problem using the "Explore Only" method.
+ * This is a naive algorithm that simply chooses a random arm each round.
  */
 public class BanditSolverExploreOnly {
   List<BanditArm> arms = new ArrayList<>();
   int totalPoints;
   int totalRounds;
   int currentRound;
+  Random random;
+
+  public BanditSolverExploreOnly(List<BanditArm> arms, int totalRounds) {
+    this.arms = arms;
+    this.totalRounds = totalRounds;
+    this.currentRound = 0;
+    this.totalPoints = 0;
+    this.random = new Random();
+  }
 
   public BanditArm selectArm() {
     BanditArm arm = new BanditArm();
     return arm;
   }
 
-  public void updateArm(BanditArm arm) {
+  public void Round(BanditArm arm) {
 
+  }
+  
+  public void runRound() {
+    
   }
 }
--- product/src/main/java/main/main.java
+++ product/src/main/java/main/main.java
@@ -3,6 +3,7 @@ package main;
 public class main {
 
   public static void main(String[] args) {
-   
+    System.out.printf("Hello World");
+
   }
 }
