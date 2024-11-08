28e074c12af4c8cc7b99419f8cc808e2677d652c
zlac480 <zlac480@live.rhul.ac.uk>
2024-11-08 21:55:08 +0000

    Finished EGreedy class, changed the selectArm() and round() methods

--- product/src/main/java/main/BanditSolverEGreedy.java
+++ product/src/main/java/main/BanditSolverEGreedy.java
@@ -2,6 +2,7 @@ package main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /*
  * This class solves the bandit problem using the "Epsilon Greedy" method.
@@ -14,26 +15,35 @@ public class BanditSolverEGreedy {
   int totalPoints;
   int totalRounds;
   int currentRound;
+  int E; 
   int bestPoints;
   BanditArm bestArm;
+  Random random;
   
   /*
    * Initialised variables above and taking list arms and totalRounds from main when set. 
    */
-  public BanditSolverEGreedy(List<BanditArm> arms, int totalRounds) {
+  public BanditSolverEGreedy(List<BanditArm> arms, int totalRounds, int E) {
     this.arms = arms;
     this.totalRounds = totalRounds;
     this.currentRound = 0;
     this.totalPoints = 0;
+    this.E = E;
     this.bestPoints = 0;
     this.bestArm = null;
+    this.random = new Random();
   }
 
   /*
    * Returns the chosen arm.
    */
   public BanditArm selectArm() {
-    
+    if (random.nextInt(101) < E) {
+      int randomIndex = random.nextInt(arms.size());
+      return arms.get(randomIndex);
+    } else {
+      return bestArm;
+    }
   }
   
   /*
@@ -58,6 +68,10 @@ public class BanditSolverEGreedy {
    */
   public void round() {
     BanditArm arm = selectArm();
+    if (arm.getPoints() > bestPoints) {
+      bestPoints = arm.getPoints(); 
+      bestArm = arm;
+    }
     arm.incrimentVisits();
     totalPoints += arm.getPoints();
     currentRound++;
