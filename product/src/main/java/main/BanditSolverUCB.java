f3c528447d3481e4fd2d73b4fac7de16ac2bd9fa
zlac480 <zlac480@live.rhul.ac.uk>
2024-11-09 16:33:32 +0000

    Finished the UCB class

--- product/src/main/java/main/BanditSolverUCB.java
+++ product/src/main/java/main/BanditSolverUCB.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 /*
- * This class solves the bandit problem using the "Epsilon Greedy" method.
- * This is an algorithm that has a value, E, set to a certain %,
- * each round there is an E% chance that we pick a random restaurant,
- * if not we go to the restaurant that has given us the best point score so far
+ * This class solves the bandit problem using the "Upper Confidence Bound (UCB)" method.
+ * UCB selects arms based on the UCB formula.
  */
 public class BanditSolverUCB {
   List<BanditArm> arms = new ArrayList<>();
+  List<Integer> totalPointsEachArm; 
   int totalPoints;
   int totalRounds;
   int currentRound;
@@ -20,27 +19,44 @@ public class BanditSolverUCB {
    */
   public BanditSolverUCB(List<BanditArm> arms, int totalRounds, int E) {
     this.arms = arms;
+    this.totalPointsEachArm = new ArrayList<>();
     this.totalPoints = 0;
     this.totalRounds = totalRounds;
     this.currentRound = 0;
+    
+    for (int i = 0; i < arms.size(); i++) {
+      totalPointsEachArm.add(0);
+    }
   }
 
   /*
-   * Returns the chosen arm.
+   * Returns the chosen arm based on the UCB algorithm.
    */
   public BanditArm selectArm() {
+    int maxUCB = 0;
+    int selectedArm = 0;
+    
+    for (int i = 0; i < arms.size(); i++) {
+      BanditArm arm = arms.get(i);
 
-  }
-  
-  /*
-   * Perform the initial exploration by visiting each arm once and finding the best.
-   */
-  public void firstRound() {
-    for (BanditArm arm : arms) {
-      arm.incrimentVisits();
-      totalPoints += arm.getPoints(); 
-      currentRound++; 
+      // If arm has not been visited before return this arm.
+      if (arm.getVisits() == 0) {
+        return arm;
+      }
+      
+      // If arm has been visited before, calculate UCB score.
+      int meanReward = totalPointsEachArm.get(i) / arm.getVisits();
+      int confidenceInterval =  (int) Math.sqrt((2 * Math.log(currentRound)) / arm.getVisits());
+      int UCBvalue = meanReward + confidenceInterval;
+      
+      // Calculate arm with highest UCB.
+      if (UCBvalue > maxUCB) {
+        maxUCB = UCBvalue;
+        selectedArm = i;
+      }
     }
+    
+    return arms.get(selectedArm);
   }
   
   /*
@@ -50,7 +66,13 @@ public class BanditSolverUCB {
   public void round() {
     BanditArm arm = selectArm();
     arm.incrimentVisits();
-    totalPoints += arm.getPoints();
+   
+    int points = arm.getPoints();
+    totalPoints += points;
+    
+    int armIndex = arms.indexOf(arm);
+    totalPointsEachArm.set(armIndex, totalPointsEachArm.get(armIndex) + points);
+    
     currentRound++;
   }
   
@@ -58,7 +80,6 @@ public class BanditSolverUCB {
    * Checks whether there are more rounds to be run and then runs them.
    */
   public void runRound() {
-    firstRound();
     while (currentRound < totalRounds) {
       round();
     }
