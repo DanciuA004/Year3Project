90eb016a38a404b2b7cdb9569afbcbb305d4c3c2
DanciuA004 <danciua004@gmail.com>
2025-01-31 11:27:36 +0000

    Set up JavaFX an beginning to do the GUI

---
+++ product/src/main/java/main/MainGui.java
@@ -0,0 +1,25 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * This class is where i will be testing the GUI for Connect4.
+ */
+public class MainGui extends Application{
+
+  /**
+   * Main.
+   *
+   * @param args args.
+   */
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage stage) throws Exception {
+
+    stage.show();
+  }
+}
