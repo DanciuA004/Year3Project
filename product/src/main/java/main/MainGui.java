32d9ba2424bdaa79790dd167d57e203c0a626452
DanciuA004 <danciua004@gmail.com>
2025-01-31 11:46:30 +0000

    Created a stage

--- product/src/main/java/main/MainGui.java
+++ product/src/main/java/main/MainGui.java
@@ -1,12 +1,15 @@
 package main;
 
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 /**
  * This class is where i will be testing the GUI for Connect4.
  */
-public class MainGui extends Application{
+public class MainGui extends Application {
 
   /**
    * Main.
@@ -19,7 +22,16 @@ public class MainGui extends Application{
 
   @Override
   public void start(Stage stage) throws Exception {
-
+    Group root = new Group();
+    Scene scene = new Scene(root, Color.BLACK);
+    
+    stage.setWidth(800);
+    stage.setHeight(800);
+    stage.setResizable(false);
+    stage.setTitle("Welcome to Connect4!");
+    
+    stage.setScene(scene);
     stage.show();
+    
   }
 }
