7487abb92158432935959c063c6b085dbd36643d
DanciuA004 <danciua004@gmail.com>
2025-03-12 12:27:12 +0000

    Planning tree node class for monte-carlo algorithm

---
+++ product/src/main/java/finalproduct/McTreeNode.java
@@ -0,0 +1,37 @@
+package finalproduct;
+
+/**
+ * This class hold the tree structure used for the monte-carlo algorithm. 
+ */
+public class McTreeNode {
+
+  /**
+   * This method uses the UCB method to pick the next best node in the tree. 
+   */
+  public void select() {
+    
+  }
+  
+  /**
+   * Expands the tree with the next possible moves, disc drop in column 0-6.
+   */
+  public void expand() {
+    
+  }
+  
+  /**
+   * For each of the next nodes added by the expand method, 
+   * multiple simulations are run on them and a value is assigned. 
+   */
+  public void simulate() {
+    
+  }
+  
+  /**
+   * Takes the data from the simulation and goes back up the tree, 
+   * updating all of the parent nodes. 
+   */
+  public void backpropogation() {
+    
+  }
+}
